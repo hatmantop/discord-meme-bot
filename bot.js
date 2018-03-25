37f10545d0feb9813331ce4bcb7b40faf85e2fc2
Teagan Horkan <tgnhrkn@gmail.com>
2018-03-24 23:52:10 -0700

    added .dog domains and php post

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ var logger = require('winston');
 var auth = require('./auth.json');
 var fs = require('fs');
 var os = require("os");
+var http = require("http");
+var querystring = require('querystring');
 
 var memeLog = fs.createWriteStream('meme_log.txt', {
     flags: 'a'
@@ -23,10 +25,11 @@ bot.on('ready', function (evt) {
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    console.log(message);
+    console.log("Guild Message: " + message);
     
     var msgParts = message.split(' ');
     if(msgParts[0].trim() == '<@427211373924057088>'){
@@ -63,12 +66,13 @@ function handleMemes(channel, msg){
     var memeCount = 0;
     for(var i = 0; i < parts.length; i++){
         var str = parts[i];
-        if(str.includes('.com') || str.includes('.net') || str.includes('.org')){
+        if(str.includes('.com') || str.includes('.net') || str.includes('.org') || str.includes('.dog')){
             memeCount++;
             if(str.includes('.jpg')){
                 if(str.indexOf('.jpg') == str.length - 4){
                     logger.info("img: " + msg);
                     memeLog.write("img: " + msg + os.EOL);
+                    postMemeInfo('img', msg);
                 }
             } else if (str.includes(".gifv")) {
                 if(str.indexOf('.gifv') == str.length - 5){
@@ -105,6 +109,43 @@ function handleMemes(channel, msg){
             message: memeAlert
         });
     }
+}
+
+function postMemeInfo(type, url) {
+    var postData = querystring.stringify({
+        'type': type,
+        'url': url
+    });
+
+    var options = {
+        hostname: '192.168.1.111',
+        port: 80,
+        path: '/meme_master/memeServer.php',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(postData)
+        }
+    };
+
+    const req = http.request(options, (res) => {
+        console.log(`STATUS ${res.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        });
+        res.on('end', () => {
+            console.log('No more data in response.');
+        });
+    });
+
+    req.on('error', (e) => {
+        console.error('problem with request: ${e.message}');
+    });
+
+    req.write(postData);
+    req.end();
 }
 
 
